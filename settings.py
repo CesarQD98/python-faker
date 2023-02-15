b575bcd4e92597f5a446765625071be895925853
Rico98 <asd.cqd98@gmail.com>
2023-02-15 10:45:44 -0500

    Relocate data folder

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
-OUTPUT_TXT_PATH = "./src/data/fake_data.txt"
+OUTPUT_TXT_PATH = "./data/fake_data.txt"
 PRODUCTO_FIELDS = ["id", "title", "price", "description", "category"]
 CATEGORIA_FIELDS = ["id", "name"]
